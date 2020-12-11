ef0b0a5763633aa7b0126cf2d23ad34156da7b00
이윤복 <iyunbog@iyunbog-ui-MacBookPro.local>
2020-12-11 22:54:11 +0900

    [개발] MemberService 주석 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,2 +1,21 @@
-package hello.core.member;public class MemberServiceImpl {
+package hello.core.member;
+
+/*
+ * MemberServiceImpl.java
+ * MemberService의 구현체
+ */
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
