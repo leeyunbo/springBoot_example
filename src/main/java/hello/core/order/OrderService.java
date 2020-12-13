676e62f63dd744870a218e3a3e511199562c4db2
leeyunbo <servers1@naver.com>
2020-12-13 22:02:36 +0900

    [개발] OrderService 구현체 구현 완료

--- src/main/java/hello/core/order/OrderService.java
+++ src/main/java/hello/core/order/OrderService.java
@@ -2,5 +2,5 @@ package hello.core.order;
 
 //public이면 외부에 공개하는 것이기 때문에 외부 파일이름과 같아야함
 public interface OrderService {
-    Order createOrder(Long member, String itemName, int itemPrice);
+    Order createOrder(Long memberId, String itemName, int itemPrice);
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,4 +1,24 @@
 package hello.core.order;
 
-public class OrderServiceImpl {
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        //멤버 등급을 체킹하기 위한 멤버 정보 호출
+        Member member = memberRepository.findById(memberId);
+
+        //할인 정책이 변경되어도 OrderService는 변경될 필요가 없음 (단일 책임의 원칙)
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
 }
