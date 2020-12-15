4203df47b145d11a190c81167fa3ba56081e366d
leeyunbo <servers1@naver.com>
2020-12-15 22:51:44 +0900

    [수정/개발] DIP, OCP를 지킬 수 있도록 AppConfig 구현 완료

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -11,7 +11,7 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
     //psvm + tab
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        MemberService memberService = new MemberServiceImpl(memberRepository);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,23 +1,18 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    // 할인 정책을 변경하려면 OrderServiceImpl 코드를 고쳐야 한다.
-    // 역할과 구현을 확실하게 분리했지만
-    // DIP를 지키지 못함(DiscountPolicy 인터페이 뿐만 아니라, RateDiscountPolicy 구현 객체에도 의존해버림)
-    // OCP를 지키지 못함(OrderServiceImpl 코드의 변경이 이루어져버림)
-    // 누가 대신 구현 객체를 생성하여 주입 시켜준다면? 이 문제가 해결됨!
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
