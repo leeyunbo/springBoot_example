474fa274cab3d8898afd5f9e89032be1713574af
leeyunbo <servers1@naver.com>
2020-12-17 21:43:46 +0900

    [개발/수정] AppConfig 리팩토링 및 정률 할인 정책으로 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,38 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+/*
+ 역할들을 잘 들어나게 구현하는 것이 중요하다. => 하나의 메서드가 하나의 역할을 가지고 있어야 함
+ => 전체가 어떻게 구성되어 있는지 보기 쉬움, 변경하기 쉬움, 중복이 제거됨
+ */
+
 public class AppConfig {
 
+    // command + option + m => 리팩토링
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // 역할을 잘 분리함으로써 쉽게 변경이 가능함(할인 정책이 바뀌는 경우).. 눈에 쉽게 들어옴
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
