004bf60a9829fda132fcae92947db85ebafe90df
leeyunbo <servers1@naver.com>
2020-12-19 22:55:27 +0900

    [수정/개발] OrderApp, MemberApp Spring Container로 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,28 +9,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /*
  역할들을 잘 들어나게 구현하는 것이 중요하다. => 하나의 메서드가 하나의 역할을 가지고 있어야 함
  => 전체가 어떻게 구성되어 있는지 보기 쉬움, 변경하기 쉬움, 중복이 제거됨
  */
 
+@Configuration
 public class AppConfig {
 
     // command + option + m => 리팩토링
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 역할을 잘 분리함으로써 쉽게 변경이 가능함(할인 정책이 바뀌는 경우).. 눈에 쉽게 들어옴
+    @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,6 +4,8 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /*
  *  MemberService 기능 테스트를 위한 클라이언트
@@ -11,8 +13,12 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
     //psvm + tab
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
