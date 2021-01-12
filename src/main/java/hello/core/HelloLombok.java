fdcc87580f83b18b8a9280e2ecbcff39c81ea442
leeyunbo <servers1@naver.com>
2021-01-12 22:15:50 +0900

    [수정] lombok을 이용해 OrderServiceImpl 리팩토링

--- src/main/java/hello/core/HelloLombok.java
+++ src/main/java/hello/core/HelloLombok.java
@@ -2,9 +2,11 @@ package hello.core;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class HelloLombok {
 
     private String name;
@@ -16,6 +18,6 @@ public class HelloLombok {
         helloLombok.setName("name");
 
         String name = helloLombok.getName();
-        System.out.println("name = " + name);
+        System.out.println("helloLombok = " + helloLombok);
     }
 }
