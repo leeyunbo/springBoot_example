0e6a3ccb1e5497f31cc1cf7d456df9e883978e36
leeyunbo <servers1@naver.com>
2021-01-14 22:14:52 +0900

    [개발] Map 주입을 이용한 다형성 및 유연성 향상

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,55 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        // 둘에서 다 빈 등록
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountServce.class);
+
+        DiscountServce dc = ac.getBean(DiscountServce.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = dc.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(dc).isInstanceOf(DiscountServce.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = dc.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+    }
+
+    static class DiscountServce {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policyList;
+
+        @Autowired
+        public DiscountServce(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+            this.policyMap = policyMap;
+            this.policyList = policyList;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policyList = " + policyList);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+
+
+    }
+}
