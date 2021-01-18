87b54ab03a5872275c117edff43227cbdda0046e
leeyunbo <servers1@naver.com>
2021-01-18 21:47:03 +0900

    [개발] 생명주기 콜백을 이용한 네트워크 시뮬레이션 개발

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -4,7 +4,6 @@ import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -49,7 +48,5 @@ public class AllBeanTest {
             DiscountPolicy discountPolicy = policyMap.get(discountCode);
             return discountPolicy.discount(member, price);
         }
-
-
     }
 }
---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,11 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+
+    }
+}
