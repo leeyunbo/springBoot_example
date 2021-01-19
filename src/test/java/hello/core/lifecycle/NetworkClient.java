1769ce54910576158e6e7f3815db15696390721f
leeyunbo <servers1@naver.com>
2021-01-19 21:06:17 +0900

    [개발] LifeCycle 콜백 메서드 추가 (InitializingBean, disposableBean)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,5 +28,20 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    // Properties의 셋팅이 끝나면 호출되는 메서드
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet()");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 파괴될 때 호출되는 메서드
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destory()");
+        disconnect();
     }
 }
