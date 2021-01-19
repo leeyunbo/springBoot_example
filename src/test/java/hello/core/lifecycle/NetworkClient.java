0c5fa54874f629f72f8eb5a160819b49be742579
leeyunbo <servers1@naver.com>
2021-01-19 21:32:50 +0900

    [수정] PostConstruct, PreDestory로 콜백메서드 설정 변경

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,10 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient  {
 
     private String url;
 
@@ -30,8 +33,39 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
+
+    /*
+     * 스프링 코드에 의존하지 않음
+     * 이름을 자유자재로 지을 수 있음
+     * 코드가 아니라 설정 정보를 사용하기에 코드를 고칠 수 없는 외부 라이브러리에도 적용할 수 있음
+     */
+    // 빈이 생성된 후 호출되는 메서드
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init()");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 파괴될 때 호출되는 메서드
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close()");
+        disconnect();
+    }
+
+
+
+
+
+
+    /*
+     * 코드 레벨에서 사용하는 인터페이스이기 때문에, 내가 고칠 수 없는 라이브러리에는 적용할 수 없음 (클래스 파일로 가져올텐데..)
+     * 초기화, 소멸 메서드의 이름을 변경할 수 없다.
+     * 모든 코드가 스프링 전용 인터페이스에 의존적으로 설계해야 함
+     */
     // Properties의 셋팅이 끝나면 호출되는 메서드
-    @Override
+/*    @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet()");
         connect();
@@ -43,5 +77,5 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     public void destroy() throws Exception {
         System.out.println("NetworkClient.destory()");
         disconnect();
-    }
+    }*/
 }
