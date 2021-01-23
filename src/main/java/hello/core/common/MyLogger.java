65c09062f87ed76afc9d8cbcb30a5ecbfe3161b1
leeyunbo <servers1@naver.com>
2021-01-23 17:43:19 +0900

    [수정] proxy mode로 수정

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,14 +1,17 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.UUID;
 
+// MyLogger를 상속받은 가짜 프록시 클래스를 만들어서 주입시킨다.
+
 @Component
-@Scope("request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     private String uuid;
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,12 +14,15 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
 
+    // provider에 의해 고객의 요청이 들어올 때 까지 request scope 빈의 생성을 지연함
+
+   // private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+        /*MyLogger myLogger = myLoggerProvider.getObject();*/
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
